c10134ee9226b1e9eceba6247101f472ec6d7056
Hey-Mynk <mail.contactmayank@gmail.com>
2024-08-02 22:28:01 +0530

    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
 
 import { AuthContextProvider } from "../context/AuthContext";
+
 export default function Home({ products }) {
   return (
     <AuthContextProvider>
@@ -28,7 +29,7 @@ export default function Home({ products }) {
   );
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps() {
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
@@ -36,5 +37,6 @@ export async function getServerSideProps(context) {
     props: {
       products,
     },
+    revalidate: 10, // Re-generate the page at most every 10 seconds
   };
 }
